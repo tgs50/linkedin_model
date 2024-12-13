d39b79c47a9c13924b33004d015d767f918a8d5b
tgs50 <tgs50@georgetown.edu>
2024-12-12 19:26:35 -0500

    Updated files to fix errors

--- linkedin_model.py
+++ linkedin_model.py
@@ -1,6 +1,8 @@
+import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 s = pd.read_csv('social_media_usage.csv')
 ss = s[['income', 'educ2', 'web1h', 'par', 'marital', 'age']]
