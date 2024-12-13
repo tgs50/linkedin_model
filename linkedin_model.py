69dc13f4ea247122de4f4e43eb1aee938a0a67c7
tgs50 <tgs50@georgetown.edu>
2024-12-12 19:46:44 -0500

    maybe now

--- linkedin_model.py
+++ linkedin_model.py
@@ -30,26 +30,11 @@ st.markdown("data defintions:")
 
 st.markdown("age (numeric age): 1 to 97")
 
-st.markdown("1	Less than high school (Grades 1-8 or no formal schooling)
-	2	High school incomplete (Grades 9-11 or Grade 12 with NO diploma)
-	3	High school graduate (Grade 12 with diploma or GED certificate)
-	4	Some college, no degree (includes some community college)
-	5	Two-year associate degree from a college or university
-	6	Four-year college or university degree/Bachelor’s degree (e.g., BS, BA, AB)
-	7	Some postgraduate or professional schooling, no postgraduate degree (e.g. some graduate school)
-	8	Postgraduate or professional degree, including master’s, doctorate, medical or law degree (e.g., MA, MS, PhD, MD, JD)")
+st.markdown("1 Less than high school, 2	High school incomplete, 3 High school graduate, 4 Some college no degree, 5 Two-year associate degree, 6 Four-year college, 7 Some postgraduate or professional schooling, 8 Postgraduate or professional degree")
 
-st.markdown("Parent: 1	Yes
-	2	No")
+st.markdown("Parent: 1 Yes, 2 No")
 
-st.markdown(current marital status
-
-	1	Married
-	2	Living with a partner
-	3	Divorced
-	4	Separated
-	5	Widowed
-	6	Never been married")
+st.markdown("current marital status: 1 Married, 2 Living with a partner, 3 Divorced,4 Separated,5 Widowed, 6 Never been married")
 
 # User input fields
 age = st.number_input("Age", min_value=1, max_value=97, step=1)
